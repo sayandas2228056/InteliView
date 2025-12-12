2682a7621bc9c0b16df5d70b536fd06ae96eb964
sayandas2228056 <offcsayantubecode@gmail.com>
2025-12-13 01:27:00 +0530

    Contact Page Fixed

--- Frontend/src/Pages/Contact.jsx
+++ Frontend/src/Pages/Contact.jsx
@@ -4,23 +4,23 @@ import Conx from '../Components/Contx';
 import { motion } from 'framer-motion';
 
 const Contact = () => {
-  return (
-    <div className="min-h-screen bg-linear-to-br from-black via-gray-800 to-black">
-      
+    return (
+        <div className="min-h-screen bg-linear-to-br from-black via-gray-900 to-black">
 
-      
-      {/* Contact Information Section */}
-      <motion.div 
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, delay: 0.4 }}
-      >
-        <Conx />
-      </motion.div>
 
-      
-    </div>
-  );
+
+            {/* Contact Information Section */}
+            <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, delay: 0.4 }}
+            >
+                <Conx />
+            </motion.div>
+
+
+        </div>
+    );
 };
 
 export default Contact;
